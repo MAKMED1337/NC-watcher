67a46ec2c5e83186190bb90aa48e9f1228bf7044
MAKMED1337 <edik.tikhoniuk@gmail.com>
2023-04-18 02:39:08 +0200

    Moved file

--- bot/connected_accounts.py
+++ bot/connected_accounts.py
@@ -1,12 +1,13 @@
 from helper.db_config import Base, db, fetch_all_column
-from sqlalchemy import Column, VARCHAR, BigInteger
+from sqlalchemy import Column, BigInteger, ForeignKey
 from sqlalchemy import select, delete, and_
 from sqlalchemy.dialects.mysql import insert
 
+#FIXME: multiple users could change key, but they need to have access only to their one
 class ConnectedAccounts(Base):
 	__tablename__ = 'ConnectedAccounts'
-	tg_id = Column(BigInteger, primary_key=True)
-	account_id = Column(VARCHAR(64), primary_key=True)
+	tg_id = Column(BigInteger, primary_key=True) #TODO: implement
+	account_id = Column(ForeignKey('Accounts.account_id', ondelete='CASCADE'), primary_key=True)
 
 	@staticmethod
 	async def is_connected(account_id: str) -> bool:
@@ -23,3 +24,7 @@ class ConnectedAccounts(Base):
 	@staticmethod
 	async def remove(tg_id: int, account_id: str):
 		await db.execute(delete(ConnectedAccounts).where(and_(ConnectedAccounts.tg_id == tg_id, ConnectedAccounts.account_id == account_id)))
+
+	@staticmethod
+	async def delete_account(account_id: str):
+		await db.execute(delete(ConnectedAccounts).where(ConnectedAccounts.account_id == account_id))
