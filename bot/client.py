118801ecd09a2b16cbc08009dff62ac01c486337
MAKMED1337 <edik.tikhoniuk@gmail.com>
2023-04-18 02:33:56 +0200

    Renamed and added delete method

--- bot/client.py
+++ bot/client.py
@@ -2,13 +2,17 @@ PORT = 2002
 
 
 
-from helper.IPC import Client
+from helper.IPC import FuncClient
 from watcher.actions import IAction
 from watcher.unpaid_rewards import UnpaidRewards
+from typing import Any
 
-class BotClient(Client):
+class BotClient(FuncClient):
 	def __init__(self):
 		super().__init__(PORT)
 
-	async def add_payment(self, action: IAction, reward: UnpaidRewards):
-		await self.send((action, reward))
+	async def notify_payment(self, action: IAction, reward: UnpaidRewards, on_exception: Any=Exception):
+		return await self.call(on_exception, 'notify_payment', action, reward)
+
+	async def delete_and_notify(self, account_id: str, on_exception: Any=Exception):
+		return await self.call(on_exception, 'delete_and_notify', account_id)
--- bot/server.py
+++ bot/server.py
@@ -1,46 +1,57 @@
 from .client import PORT
 
-from helper.IPC import Server, Connection
+from helper.IPC import Server, Connection, Packet, Response, FuncCall
 from helper.report_exceptions import report_exception
 from helper.bot_config import bot
 
 from watcher.actions import IAction, get_payment_cost, modes, qualities
 from watcher.unpaid_rewards import UnpaidRewards, ActionEnum
+from .connected_accounts import ConnectedAccounts
 import html
 
 server = Server(PORT, Connection, report_exception)
 
+async def delete_and_notify(action: IAction, reward: UnpaidRewards):
+	print(action, reward)
+	assert isinstance(action, IAction), type(action)
+	#assert isinstance(reward, UnpaidRewards), type(reward)       <- 'sqlalchemy.engine.row.Row'
+	action: IAction
+	reward: UnpaidRewards
+
+	#text = f'raw:\n\n<code>{reward.tx_id}</code>\ncost: <code>{get_payment_cost(reward)}</code>\n\n<pre>{reward._mapping}</pre>\n\n<pre>{asdict(action.info)}</pre>'
+	#await bot.send_message('@makmed1337', text[:4096])
+
+	info = action.info
+	text = f'TX: <code>{reward.tx_id}</code>\n\n'
+	text += f'Account: <code>{reward.account_id}</code>\n'
+	text += f'Action: {reward.action.name.capitalize()}\n'
+	text += f'{modes[info.mode].name}({info.task_id}): <pre>{html.escape(info.short_descr)}</pre>\n\n'
+
+	if reward.action == ActionEnum.review:
+		text += 'Your verdict: <b>' + ('Rejected' if info.my_verdict == 0 else qualities[info.my_quality]) + '</b>'
+		text += ', final verdict: <b>' + ('Rejected' if info.status == 3 else qualities[info.quality]) + '</b>\n'
+		text += f'Your comment: <pre>{html.escape(action.get_my_review()["comment"])}</pre>\n\n'
+	else:
+		text += 'Verdict: <b>' + ('Rejected' if info.status == 3 else qualities[info.quality]) + '</b>\n'
+		text += f'Comment: <pre>{html.escape(info.reviews[-1]["comment"])}</pre>\n\n'
+		text += f'Resubmits: <b>{info.resubmits}</b>\n\n'
+
+	text += f'Price: <b>{get_payment_cost(reward) / 1000}</b>Ⓝ'
+	await bot.send_message('@makmed1337', text[:4096])
+
+async def delete_account(account_id: str):
+	await ConnectedAccounts.delete_account(account_id)
+	await bot.send_message('@makmed1337', f'Account was deleted: <code>{account_id}</code>')
+
 @server.on_connect
 async def on_client_connect(conn: Connection):
 	while conn.is_active():
-		msg = await conn.read(None)
-		if msg is None:
+		packet: Packet = await conn.read(None)
+		if packet is None:
 			break
-		
-		action, reward = msg
-		print(action, reward)
-		assert isinstance(action, IAction), type(action)
-		#assert isinstance(reward, UnpaidRewards), type(reward)       <- 'sqlalchemy.engine.row.Row'
-		action: IAction
-		reward: UnpaidRewards
 
-		#text = f'raw:\n\n<code>{reward.tx_id}</code>\ncost: <code>{get_payment_cost(reward)}</code>\n\n<pre>{reward._mapping}</pre>\n\n<pre>{asdict(action.info)}</pre>'
-		#await bot.send_message('@makmed1337', text[:4096])
+		call: FuncCall = packet.data
+		assert call.name in ('notify_payment', 'delete_and_notify')
 
-		info = action.info
-		text = f'TX: <code>{reward.tx_id}</code>\n\n'
-		text += f'Account: <code>{reward.account_id}</code>\n'
-		text += f'Action: {reward.action.name.capitalize()}\n'
-		text += f'{modes[info.mode].name}({info.task_id}): <pre>{html.escape(info.short_descr)}</pre>\n\n'
-
-		if reward.action == ActionEnum.review:
-			text += 'Your verdict: <b>' + ('Rejected' if info.my_verdict == 0 else qualities[info.my_quality]) + '</b>'
-			text += ', final verdict: <b>' + ('Rejected' if info.status == 3 else qualities[info.quality]) + '</b>\n'
-			text += f'Your comment: <pre>{html.escape(action.get_my_review()["comment"])}</pre>\n\n'
-		else:
-			text += 'Verdict: <b>' + ('Rejected' if info.status == 3 else qualities[info.quality]) + '</b>\n'
-			text += f'Comment: <pre>{html.escape(info.reviews[-1]["comment"])}</pre>\n\n'
-			text += f'Resubmits: <b>{info.resubmits}</b>\n\n'
-
-		text += f'Price: <b>{get_payment_cost(reward) / 1000}</b>Ⓝ'
-		await bot.send_message('@makmed1337', text[:4096])
+		resp = Response(conn, packet)
+		await resp.respond(globals()[call.name])
