8d8657585be8e7b51fdccc9fe07ee9a57fadd002
MAKMED1337 <edik.tikhoniuk@gmail.com>
2023-05-20 01:40:36 +0200

    Fixed test

--- bot/test.py
+++ bot/test.py
@@ -10,21 +10,21 @@ async def main():
 	p = BotClient()
 	async with p:
 		list_info = ListTaskInfo({'mode': 750, 'user_task_id': 0, 'my_quality': 0, 'my_verdict': 0, 'quality': 0, 'short_descr': 'TEST', 'status': 0})
-		task_info = TaskInfo(list_info, {'resubmits': 0, 'reward': 0, 'reviews': [{'mine': True, 'comment': 'TEST'}]})
+		task_info = TaskInfo(list_info, {'resubmits': 0, 'reward': 0, 'reviews': [{'mine': True, 'comment': 'TEST'}], 'comment': 'IDK'})
 
 		r = Review()
 		r.info = task_info
 
 		async with db.transaction():
 			await UnpaidRewards.add('TEST', 'TEST', 0, 0, ActionEnum.review, 0)
-			await p.notify_payment(r, (await UnpaidRewards.get('TEST', ActionEnum.review))[0])
+			await p.notify_payment(r, (await UnpaidRewards.get('TEST', ActionEnum.review))[0]._mapping)
 			await UnpaidRewards.clear('TEST')
 
 		t = Task()
 		t.info = task_info
 		async with db.transaction():
 			await UnpaidRewards.add('TEST', 'TEST', 0, 0, ActionEnum.task, 0)
-			await p.notify_payment(r, (await UnpaidRewards.get('TEST', ActionEnum.task))[0])
+			await p.notify_payment(r, (await UnpaidRewards.get('TEST', ActionEnum.task))[0]._mapping)
 			await UnpaidRewards.clear('TEST')
 
 if __name__ == '__main__':
