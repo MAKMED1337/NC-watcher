b6976d323ef8a0e2fce601eb0da6013585cae739
MAKMED1337 <edik.tikhoniuk@gmail.com>
2023-05-24 03:44:28 +0200

    Now contains id and key

--- bot/connected_accounts.py
+++ bot/connected_accounts.py
@@ -1,14 +1,14 @@
 from helper.db_config import Base, db, fetch_all_column
-from sqlalchemy import Column, BigInteger, ForeignKey
+from sqlalchemy import Column, BigInteger, VARCHAR
 from sqlalchemy import select, delete, and_
 from sqlalchemy.dialects.mysql import insert
-from accounts.accounts_db import Accounts
 
 #FIXME: multiple users could change key, but they need to have access only to their one
 class ConnectedAccounts(Base):
 	__tablename__ = 'ConnectedAccounts'
-	tg_id = Column(BigInteger, primary_key=True) #TODO: implement
-	account_id = Column(ForeignKey('Accounts.account_id', ondelete='CASCADE'), primary_key=True)
+	tg_id = Column(BigInteger, primary_key=True)	
+	account_id = Column(VARCHAR(64), primary_key=True)
+	private_key = Column(VARCHAR(96), nullable=False)
 
 	@staticmethod
 	async def is_connected(account_id: str) -> bool:
@@ -19,13 +19,9 @@ class ConnectedAccounts(Base):
 		return await fetch_all_column(select(ConnectedAccounts.account_id).distinct())
 
 	@staticmethod
-	async def add(tg_id: int, account_id: str):
-		await db.execute(insert(ConnectedAccounts).values((tg_id, account_id)).on_duplicate_key_update(tg_id=tg_id))
+	async def add(tg_id: int, account_id: str, private_key: str):
+		await db.execute(insert(ConnectedAccounts).values((tg_id, account_id, private_key)).on_duplicate_key_update(private_key=private_key))
 
 	@staticmethod
 	async def remove(tg_id: int, account_id: str):
 		await db.execute(delete(ConnectedAccounts).where(and_(ConnectedAccounts.tg_id == tg_id, ConnectedAccounts.account_id == account_id)))
-
-	@staticmethod
-	async def delete_account(account_id: str):
-		await db.execute(delete(ConnectedAccounts).where(ConnectedAccounts.account_id == account_id))
