c5332908b044fb3b239bbadc4ec06d35dd9536e9
MAKMED1337 <edik.tikhoniuk@gmail.com>
2023-06-20 04:36:31 +0200

    Add tests for matcher

---
+++ testset/test_matcher.py
@@ -0,0 +1,74 @@
+from watcher.payments_matcher import match_payments
+from watcher.unpaid_rewards import UnpaidRewards, ActionEnum
+from watcher.actions import *
+from testset.test_task_cost import create_task
+from testset.test_review_cost import create_review
+import pytest
+
+def is_matching(actions: list[IAction], rewards: list[UnpaidRewards]) -> bool:
+	return match_payments(actions, rewards) == list(zip(actions, rewards))
+
+def test_matcher_empty():
+	assert match_payments([], []) == [] #match
+
+def test_matcher_different_size():
+	assert match_payments([], [UnpaidRewards()]) is None
+	assert match_payments([IAction()], []) is None
+	assert match_payments([IAction(), IAction()], [UnpaidRewards()]) is None
+	assert match_payments([IAction()], [UnpaidRewards(), UnpaidRewards()]) is None
+
+@pytest.mark.asyncio
+async def test_matcher_task_1_ok():
+	task = await create_task(18, 1, 2, 10, 0, 740, [])
+
+	reward = UnpaidRewards(cost=814, coef=1.1, action=ActionEnum.task, adjustment=1)
+	assert is_matching([task], [reward])
+
+	reward = UnpaidRewards(cost=1017, coef=1.1, action=ActionEnum.task, adjustment=1)
+	assert is_matching([task], [reward])
+
+@pytest.mark.asyncio
+async def test_matcher_task_1_bad():
+	task = await create_task(18, 1, 2, 10, 0, 740, [])
+	
+	assert match_payments([task], [UnpaidRewards(cost=611, coef=1.1, action=ActionEnum.task, adjustment=1)]) is None
+	assert match_payments([task], [UnpaidRewards(cost=0, coef=1.1, action=ActionEnum.task, adjustment=0)]) is None
+	assert match_payments([task], [UnpaidRewards(cost=1000, coef=1.1, action=ActionEnum.task, adjustment=1)]) is None
+
+@pytest.mark.asyncio
+async def test_matcher_review_1_ok():
+	review = await create_review(18, 1, 1, 0, 300)
+	reward = UnpaidRewards(cost=330, coef=1.1, action=ActionEnum.review, adjustment=1)
+	assert is_matching([review], [reward])
+
+	review = await create_review(18, 0, 0, 0, 300)
+	reward = UnpaidRewards(cost=330, coef=1.1, action=ActionEnum.review, adjustment=1)
+	assert is_matching([review], [reward])
+	
+@pytest.mark.asyncio
+async def test_matcher_review_1_bad():
+	review = await create_review(18, 1, 1, 0, 300)
+	reward = UnpaidRewards(cost=440, coef=1.1, action=ActionEnum.review, adjustment=1)
+	assert match_payments([review], [reward]) is None
+
+	review = await create_review(18, 0, 0, 0, 300)
+	reward = UnpaidRewards(cost=440, coef=1.1, action=ActionEnum.review, adjustment=1)
+	assert match_payments([review], [reward]) is None
+
+@pytest.mark.asyncio
+async def test_matcher_review_2_ok():
+	review1 = await create_review(18, 1, 1, 0, 300)
+	reward1 = UnpaidRewards(cost=330, coef=1.1, action=ActionEnum.review, adjustment=1)
+
+	review2 = await create_review(18, 0, 0, 0, 400)
+	reward2 = UnpaidRewards(cost=440, coef=1.1, action=ActionEnum.review, adjustment=1)
+	assert is_matching([review1, review2], [reward1, reward2])
+
+@pytest.mark.asyncio
+async def test_matcher_review_2_bad():
+	review1 = await create_review(18, 1, 1, 0, 300)
+	reward1 = UnpaidRewards(cost=440, coef=1.1, action=ActionEnum.review, adjustment=1)  #random number
+
+	review2 = await create_review(18, 0, 0, 0, 400)
+	reward2 = UnpaidRewards(cost=440, coef=1.1, action=ActionEnum.review, adjustment=1)
+	assert match_payments([review1, review2], [reward1, reward2]) is None
