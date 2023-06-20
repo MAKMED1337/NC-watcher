1802e7c8d713bc202087ccb3feb81700c0f64a4d
MAKMED1337 <edik.tikhoniuk@gmail.com>
2023-06-20 04:10:31 +0200

    Create function to create review normally

--- testset/test_review_cost.py
+++ testset/test_review_cost.py
@@ -2,7 +2,7 @@ from testset.fake_accounts_client import *
 from watcher.actions import *
 import pytest
 
-def create_review(mode: int, verdict: int, final_verdict: int | None, before_resubmit: bool, reward: int) -> Info: 
+def create_review_info(mode: int, verdict: int, final_verdict: int | None, before_resubmit: bool, reward: int) -> Info: 
 	if final_verdict is None:
 		status = 1
 	else:
@@ -11,13 +11,15 @@ def create_review(mode: int, verdict: int, final_verdict: int | None, before_res
 	            reviews=[make_review(verdict, before_resubmit, True)],
 	            pillar=None, resubmits=0, ideas=[])
 
+async def create_review(mode: int, verdict: int, final_verdict: int | None, before_resubmit: bool, reward: int) -> Review:
+	info = create_review_info(mode, verdict, final_verdict, before_resubmit, reward)
+	account = FakeSingleAccount([info])
+	return await Review.load(account, (await account.get_task_list(info.mode))[0])
+
 #my, final
 @pytest.mark.asyncio
 async def test_AC_AC():
-	info = create_review(18, 1, 1, 0, 300)
-	account = FakeSingleAccount([info])
-
-	r = await Review.load(account, (await account.get_task_list(info.mode))[0])
+	r = await create_review(18, 1, 1, 0, 300)
 	assert r.has_ended()
 	assert feq(r.calculate_cost(), 300)
 
@@ -26,28 +28,19 @@ async def test_AC_AC():
 
 @pytest.mark.asyncio
 async def test_AC_RJ():
-	info = create_review(18, 1, 0, 1, 300)
-	account = FakeSingleAccount([info])
-
-	r = await Review.load(account, (await account.get_task_list(info.mode))[0])
+	r = await create_review(18, 1, 0, 1, 300)
 	assert r.has_ended()
 	assert feq(r.calculate_cost(), 0)
 
 @pytest.mark.asyncio
 async def test_RJ_AC():
-	info = create_review(18, 0, 1, 0, 300)
-	account = FakeSingleAccount([info])
-
-	r = await Review.load(account, (await account.get_task_list(info.mode))[0])
+	r = await create_review(18, 0, 1, 0, 300)
 	assert r.has_ended()
 	assert feq(r.calculate_cost(), 0)
 
 @pytest.mark.asyncio
 async def test_RJ_RJ():
-	info = create_review(18, 0, 0, 0, 300)
-	account = FakeSingleAccount([info])
-
-	r = await Review.load(account, (await account.get_task_list(info.mode))[0])
+	r = await create_review(18, 0, 0, 0, 300)
 	assert r.has_ended()
 	assert feq(r.calculate_cost(), 300)
 
@@ -56,16 +49,10 @@ async def test_RJ_RJ():
 
 @pytest.mark.asyncio
 async def test_unended_review_AC():
-	info = create_review(18, 1, None, 0, 3500)
-	account = FakeSingleAccount([info])
-
-	r = await Review.load(account, (await account.get_task_list(info.mode))[0])
+	r = await create_review(18, 1, None, 0, 3500)
 	assert not r.has_ended()
 
 @pytest.mark.asyncio
 async def test_unended_review_RJ():
-	info = create_review(18, 0, None, 0, 3500)
-	account = FakeSingleAccount([info])
-
-	r = await Review.load(account, (await account.get_task_list(info.mode))[0])
+	r = await create_review(18, 0, None, 0, 3500)
 	assert not r.has_ended()
