78a433c91d6df3924e7d2cf373da0bd45badb5b0
MAKMED1337 <edik.tikhoniuk@gmail.com>
2023-06-21 04:16:51 +0200

    Fix test

--- bot/test.py
+++ bot/test.py
@@ -1,31 +1,32 @@
 from .client import BotClient
-from watcher.actions import Review, Task, TaskInfo
+from watcher.actions import Review, Task, FullTaskInfo
 from watcher.unpaid_rewards import UnpaidRewards, ActionEnum
 import asyncio
 from helper.db_config import db, start as db_start
-from accounts.client import ListTaskInfo
+from accounts.client import ListTaskInfo, InnerTaskInfo
 
 async def main():
 	await db_start()
 	p = BotClient()
 	async with p:
 		list_info = ListTaskInfo({'mode': 750, 'user_task_id': 0, 'my_quality': 0, 'my_verdict': 0, 'quality': 0, 'short_descr': 'TEST', 'status': 0})
-		task_info = TaskInfo(list_info, {'resubmits': 0, 'reward': 0, 'reviews': [{'mine': True, 'comment': 'TEST'}], 'comment': 'IDK'})
+		inner_task_info = InnerTaskInfo({'resubmits': 0, 'reward': 0, 'reviews': [{'mine': True, 'comment': 'TEST', 'verdict': 0}], 'comment': 'IDK'})
+		task_info = FullTaskInfo(list_info, inner_task_info)
 
 		r = Review()
 		r.info = task_info
 
 		async with db.transaction():
-			await UnpaidRewards.add('TEST', 'TEST', 0, 0, ActionEnum.review, 0)
-			await p.notify_payment(r, (await UnpaidRewards.get('TEST', ActionEnum.review))[0]._mapping)
-			await UnpaidRewards.clear('TEST')
+			await UnpaidRewards.add('TEST', 'makmed1337.near', 0, 0, ActionEnum.review, 1)
+			await p.notify_payment(r, (await UnpaidRewards.get('makmed1337.near', ActionEnum.review))[0]._mapping)
+			await UnpaidRewards.clear('makmed1337.near')
 
 		t = Task()
 		t.info = task_info
 		async with db.transaction():
-			await UnpaidRewards.add('TEST', 'TEST', 0, 0, ActionEnum.task, 0)
-			await p.notify_payment(r, (await UnpaidRewards.get('TEST', ActionEnum.task))[0]._mapping)
-			await UnpaidRewards.clear('TEST')
+			await UnpaidRewards.add('TEST', 'makmed1337.near', 0, 0, ActionEnum.task, 0)
+			await p.notify_payment(r, (await UnpaidRewards.get('makmed1337.near', ActionEnum.task))[0]._mapping)
+			await UnpaidRewards.clear('makmed1337.near')
 
 if __name__ == '__main__':
 	asyncio.run(main())
