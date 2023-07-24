20d67d63390f3627e08ac39f1eb3fe2011ab9734
MAKMED1337 <edik.tikhoniuk@gmail.com>
2023-07-24 04:22:54 +0200

    Process all unprocessed blocks

--- watcher/block_retriever.py
+++ watcher/block_retriever.py
@@ -32,6 +32,10 @@ async def get_block_by_id(id: int) -> dict | None:
 	
 	print('smth wrong with block', id)
 
+def split_into_chunks(lst: list, n: int):
+	for i in range(0, len(lst), n):
+		yield lst[i:i + n]
+
 #unordered
 async def retrieve_new_blocks(process_block: Callable[[dict | None], None]):
 	block = await auto_retry(provider.get_block, finality=FinalityTypes.FINAL)
@@ -51,10 +55,9 @@ async def retrieve_new_blocks(process_block: Callable[[dict | None], None]):
 	async def load_and_process(block_id: int):
 		block = await get_block_by_id(block_id)
 		if block is None:
-			return
-		await process_block(block)
+			await ProcessedBlocks.set_processed(block_id) # Don't process zombie blocks
+		else:
+			await process_block(block)
 
-	processing_last = asyncio.create_task(process_block(block))
-	for i in range(last_processed_block + 1, last_block, COROS_LIMIT): #to reduce memory usage
-		await wait_pool([load_and_process(id) for id in range(i, min(i + COROS_LIMIT, last_block))])
-	await processing_last
+	for chunk in split_into_chunks(await ProcessedBlocks.get_unprocessed(), COROS_LIMIT): #to reduce memory usage
+		await wait_pool([load_and_process(id) for id in chunk])
