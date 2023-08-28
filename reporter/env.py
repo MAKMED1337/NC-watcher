bd810bcd2ab8b3e8ec3dffe560609e09f8d55e2b
MAKMED1337 <edik.tikhoniuk@gmail.com>
2023-08-28 03:11:49 +0200

    Fix `env` for `reporter`

--- reporter/env.py
+++ reporter/env.py
@@ -4,6 +4,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Env(BaseSettings):
     model_config = SettingsConfigDict(env_file='env/reporter.env')
 
+    TOKEN: str  # FIXME: it's not neccesary field, but won't work without it
+    BOT_NAME: str  # FIXME: it's not neccesary field, but won't work without it
     group_id: int
 
 
