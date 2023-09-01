d8a04c39b86a9036693dea45bbad07bfe454ddba
MAKMED1337 <edik.tikhoniuk@gmail.com>
2023-09-01 12:44:23 +0000

    Move backups into folder

--- create_backup.bash
+++ create_backup.bash
@@ -1,1 +1,1 @@
-mysqldump watcher_dev --single-transaction --quick --lock-tables=false > backup-dev-$(date +%"F-%T").sql
+mysqldump watcher_dev --single-transaction --quick --lock-tables=false > backup/dev-$(date +%"F-%T").sql
